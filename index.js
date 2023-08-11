9a10ec629a0a4eed6b6d6d7613f2a3cb50b4c208
mateteCode <matete@gmail.com>
2023-08-11 02:38:19 -0300

    refactor: change folder structure and install ejs

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const userRouter = require("./routes/userRouter");
+const userRouter = require("./src/routes/userRouter");
 
 const PORT = process.env.PORT || 3000;
 //express.static();
 
 app
+  .set("view engine", "ejs")
+  .set("views", "__dirname/src/views")
   .use(express.urlencoded({ extended: false }))
   .use("/users", userRouter)
   .listen(PORT, () => {
